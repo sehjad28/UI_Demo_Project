f735692b635dcf30ea3a18f04cc05a37ffb30439
Sehjad Khan <AL2436@accionlabs.in>
2023-01-31 20:07:06 +0530

    POM ALL updated

--- src/main/java/com/project/pages/home/HomePage.java
+++ src/main/java/com/project/pages/home/HomePage.java
@@ -1,11 +1,9 @@
 package com.project.pages.home;
 
-import com.cinch.testautomation.ui.base.BasePage;
-import com.cinch.testautomation.ui.utilities.Wait;
+import com.testautomation.ui.base.BasePage;
+import com.testautomation.ui.utilities.Wait;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
 
@@ -16,7 +14,7 @@ public class HomePage extends BasePage {
 
     Wait wait = new Wait();
 
-    @Step("Go to Agent Site...")
+    @Step("Go to Site...")
     public void gotoSite(String url) throws Exception {
         gotoURL(url);
     }
--- src/test/java/com/project/home/HomePageTest.java
+++ src/test/java/com/project/home/HomePageTest.java
@@ -1,9 +1,9 @@
 package com.project.home;
 
-import com.cinch.testautomation.ui.base.BaseTest;
-import com.cinch.testautomation.ui.reporting.TestListener;
-import com.cinch.testautomation.ui.security.ZapScan;
 import com.project.pages.home.HomePage;
+import com.testautomation.ui.base.BaseTest;
+import com.testautomation.ui.reporting.TestListener;
+import com.testautomation.ui.security.ZapScan;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -12,13 +12,16 @@ public class HomePageTest extends BaseTest {
 
     @Test
     public void test() throws Exception {
-        String my_url = "http://yahoo.com";
-        //String my_url = "http://php.testsparker.com/process.php?file=Generics/index.nsp";
+       // String my_url = "https://www.hackthissite.org/";
+        String my_url = "http://php.testsparker.com/process.php?file=Generics/index.nsp";
         // page.getPageInstance(HomePage.class).gotoSite("https://customer.qa.accelerate.cinchhs.com/account");
         page.getPageInstance(HomePage.class).gotoSite(my_url);
+        Thread.sleep(10000);
         System.out.println("ZAP SCAN TEST" + driver.getCurrentUrl());
         Thread.sleep(5000);
         ZapScan.passiveScan(driver.getCurrentUrl());
+        ZapScan.spider(driver.getCurrentUrl());
+        ZapScan.activeScan(driver.getCurrentUrl());
 
 
     }
