1fc29e3160f1104b46c172177b9de34a4fd5b12e
Sehjad Khan <AL2436@accionlabs.in>
2023-02-08 13:05:14 +0530

    with version 1.0.8

--- src/test/java/com/project/home/HomePageTest.java
+++ src/test/java/com/project/home/HomePageTest.java
@@ -13,15 +13,18 @@ public class HomePageTest extends BaseTest {
     @Test
     public void test() throws Exception {
        // String my_url = "https://www.hackthissite.org/";
-        String my_url = "http://php.testsparker.com/process.php?file=Generics/index.nsp";
+        String my_url = "http://php.testsparker.com";
+      //  String my_url = "http://192.168.0.109/WackoPicko/";
         // page.getPageInstance(HomePage.class).gotoSite("https://customer.qa.accelerate.cinchhs.com/account");
         page.getPageInstance(HomePage.class).gotoSite(my_url);
-        Thread.sleep(10000);
-        System.out.println("ZAP SCAN TEST" + driver.getCurrentUrl());
+        Thread.sleep(6000);
+        System.out.println("ZAP SCAN TEST :: " + driver.getCurrentUrl());
         Thread.sleep(5000);
-        ZapScan.passiveScan(driver.getCurrentUrl());
-        ZapScan.spider(driver.getCurrentUrl());
-        ZapScan.activeScan(driver.getCurrentUrl());
+    //    ZapScan.spider(my_url);
+//        Thread.sleep(5000);
+         ZapScan.passiveScan(my_url);
+//        Thread.sleep(5000);
+   //     ZapScan.activeScan(my_url);
 
 
     }
