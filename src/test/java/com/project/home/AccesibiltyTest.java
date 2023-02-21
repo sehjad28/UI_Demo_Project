297ef48900be34fd00141574ec8c1449bdf61729
Sehjad Khan <AL2436@accionlabs.in>
2023-02-21 21:50:34 +0530

    pom xml

---
+++ src/test/java/com/project/home/AccesibiltyTest.java
@@ -0,0 +1,30 @@
+package com.project.home;
+
+import com.project.pages.home.HomePage;
+import com.testautomation.ui.axe.CheckAxe;
+import com.testautomation.ui.base.BaseTest;
+import com.testautomation.ui.reporting.TestListener;
+import com.testautomation.ui.utilities.CustomSoftAssert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+@Listeners({ TestListener.class })
+public class AccesibiltyTest extends BaseTest {
+
+    SoftAssert softAssert = new SoftAssert();
+    @Test
+    public void test() throws Exception {
+        // String my_url = "https://www.hackthissite.org/";
+        String my_url = "http://php.testsparker.com";
+        //String my_url = "http://192.168.0.109/WackoPicko/";
+        //page.getPageInstance(HomePage.class).gotoSite("https://customer.qa.accelerate.cinchhs.com/account");
+        page.getPageInstance(HomePage.class).gotoSite(my_url);
+        Thread.sleep(6000);
+        CustomSoftAssert.axeSoftAssert(CheckAxe.checkAxe(driver), softAssert);
+        softAssert.assertAll();
+
+    }
+
+
+}
